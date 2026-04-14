07a0e643133b488d4935df6e61fc7154e12a341d
Sai Shanmukha <nss10@outlook.com>
2026-04-14 01:35:12 -0500

    Add source code and dhathu data

---
+++ src/dhathu/dhathu_split.py
@@ -0,0 +1,46 @@
+import json
+import os
+from src.models.Dhathu import Dhathu, DhathuForm, DhathuRupa
+
+def populate_dhathu_object_dict(base_file_path):
+    return {dhathu.base_index: dhathu for dhathu in (Dhathu(raw_data) for raw_data in json.load(open(base_file_path, "r"))["data"])}
+
+def dump_dhathu_base_csv(dhathu_object_dict):
+    # Print Base information of all dhatus in csv format
+    with open("data/dhathu/dhathu_base.csv", "w") as f:
+        f.write(Dhathu.base_csv_column_names() + "\n")
+        for _, dhathu in dhathu_object_dict.items():
+            f.write(dhathu.to_base_csv_string() + "\n")
+
+def populate_dhathu_lakaar(dhathu_object_dict, form, file_path):
+    lakaar_collection = json.load(open(file_path, "r"))
+    for base_index, lakaar_dict in lakaar_collection.items():
+        dhathu_object_dict[base_index].dhathu_forms[form] = DhathuForm(form, lakaar_dict)
+
+def dump_dhathu_lakaar_csv(dhathu_object_dict, form):
+    with open(f"data/dhathu/{form}.csv", "w") as f:
+        f.write(f"{Dhathu.lakaar_csv_column_names()}\n")
+        for _, dhathu in dhathu_object_dict.items():
+                dhathu_form = dhathu.dhathu_forms.get(form)
+                if dhathu_form:
+                    for lakaar_code, dhathu_rupa in dhathu_form.lakaar_dict.items():
+                        f.write(dhathu.to_lakaar_csv_string(lakaar_code, dhathu_rupa) + "\n")
+
+def create_lakaar_excel_workbook(dhathu_object_dict, ):
+    #TODO : Create an excel workbook with each sheet representing a Dhathuform
+    #       and containing the corresponding dhatus and all its lakaars
+    # Currently creating a separate csv for each lakaar form, 
+    # and copying them to excel manually. This can be automated in the future.
+    pass
+
+
+if __name__ == "__main__":
+
+    base_file_path = os.path.expanduser("~/Documents/git_projects_personal/data/dhatu/data.txt")
+    dhathu_object_dict = populate_dhathu_object_dict(base_file_path)
+    dump_dhathu_base_csv(dhathu_object_dict)
+    for form in Dhathu.DHATHU_FORM_NAMES:
+        file_path =  os.path.expanduser(f"~/Documents/git_projects_personal/data/dhatu/dhatuforms_vidyut_{form}.txt")
+        populate_dhathu_lakaar(dhathu_object_dict, form, file_path)
+        dump_dhathu_lakaar_csv(dhathu_object_dict, form)
+    create_lakaar_excel_workbook(dhathu_object_dict)
---
+++ src/models/Dhathu.py
@@ -0,0 +1,150 @@
+class Dhathu:
+    DHATHU_FORM_NAMES = [
+            "shuddha_kartari",
+            "shuddha_karmani",
+            "san_kartari",
+            "san_karmani",
+            "nich_kartari",
+            "nich_karmani",
+            "yang_kartari",
+            "yang_karmani",
+            "yangluk_kartari",
+            "yangluk_karmani",
+        ]
+
+    def __init__(self, raw_data):
+        self.raw_data = raw_data
+        self.id = raw_data["i"]
+        self.base_index = raw_data["baseindex"]
+        self.chapter = int(self.base_index.split(".")[0])
+        self.index = int(self.base_index.split(".")[1])
+        self.name = raw_data["dhatu"]
+        self.aupadeshik = raw_data["aupadeshik"]
+        self.ganam = self._get_ganam(raw_data["gana"])
+        self.padam = self._get_padam(raw_data["pada"])
+        self.settva = self._get_settva(raw_data["settva"])
+        self.karma = self._get_karma(raw_data["karma"])
+        self.kaumudi_dhatu_number = raw_data["sk"]
+        self.meaning = raw_data["artha"]
+        self.english_meaning = raw_data["artha_english"]
+        self.hindi_meaning = raw_data["artha_hindi"]
+        self.dhathu_forms = {}
+
+
+    @classmethod
+    def base_csv_column_names(cls):
+        return "id;chapter;index;name;aupadeshik;ganam;padam;settva;karma;kaumudi_dhatu_number;meaning;english_meaning;hindi_meaning"
+
+    @classmethod
+    def lakaar_csv_column_names(cls):
+        return "id;chapter;index;name;lakaar_name;prathama_eka;prathama_dvi;prathama_bahu;madhyama_eka;madhyama_dvi;madhyama_bahu;uttama_eka;uttama_dvi;uttama_bahu;"
+
+    def to_base_csv_string(self):
+        return f"{self.id};{self.chapter};{self.index};{self.name};{self.aupadeshik};{self.ganam};{self.padam};{self.settva};{self.karma};{self.kaumudi_dhatu_number};{self.meaning};{self.english_meaning};{self.hindi_meaning}"
+    
+    
+    def to_lakaar_csv_string(self, lakaar_code, dhathu_rupa):
+        return f"{self.id};{self.chapter};{self.index};{self.name};{dhathu_rupa.lakaar_name};{dhathu_rupa.value}"
+
+    def _get_karma(self, karma_letter):
+        karma_dict = {
+            "S": "सकर्मकः",
+            "A": "अकर्मकः",
+            "D": "द्विकर्मकः",
+        }
+        return karma_dict.get(karma_letter, "")
+    
+    def _get_settva(self, settva_letter):
+        settva_dict = {
+            "S": "सेट्",
+            "A": "अनिट्",
+            "V": "वेट्",
+        }
+        return settva_dict.get(settva_letter, "")
+    def _get_ganam(self, gana_number):
+        gana_dict = {
+            "1": "भ्वादि",
+            "2": "अदादि",
+            "3": "जुहोतादि",
+            "4": "दिवादि",
+            "5": "स्वादि",
+            "6": "तुदादि", 
+            "7": "रुधादि",
+            "8": "तनादि",
+            "9": "क्र्यादि",
+            "10": "चुरादि",
+        }
+        return gana_dict.get(gana_number, "")
+    
+    def _get_padam(self, pada_letter):
+        pada_dict = {
+            "P": "परस्मैपदी",
+            "A": "आत्मनेपदी",
+            "U": "उभयपदी",
+        }
+        return pada_dict.get(pada_letter, "")
+
+
+
+class DhathuForm:
+    def __init__(self, form_code, lakaar_dict):
+        self.form_code = form_code
+        self.form_name = self._dhathu_form_name(form_code)
+        self.set_lakaar_dict(lakaar_dict)
+    
+    def set_lakaar_dict(self, lakaar_dict):
+        self.lakaar_dict = {lakaar_code: DhathuRupa(lakaar_code, value) for lakaar_code, value in lakaar_dict.items()}
+    
+    def _dhathu_form_name(self, form_code):
+        form_dict = {
+            "kartari": "कर्तरि",
+            "yak": "भावकर्मणोः",
+            "san": "सन्नन्ते - कर्तरि",
+            "san_yak": "सन्नन्ते - भावकर्मणोः",
+            "nich": "णिजन्ते - कर्तरि",
+            "nich_yak": "णिजन्ते - भावकर्मणोः",
+            "yang": "यङन्ते - कर्तरि",
+            "yang_yak": "यङन्ते - भावकर्मणोः",
+            "yangluk": "यङ्लुगन्ते - कर्तरि",
+            "yangluk_yak": "यङ्लुगन्ते - भावकर्मणोः",
+        }
+        return form_dict.get(form_code, "")
+
+class DhathuRupa:
+    def __init__(self, lakaar_code, value):
+        self.value = value
+        self.lakaar_code = lakaar_code
+        self.lakaar_name = DhathuRupa._get_lakaar_name(lakaar_code)
+
+    @classmethod
+    def csv_column_names(cls):
+        return "lakaar_code;lakaar_name;plat;plit;plut;plrut;plet;plot;plang;pvidhiling;pashirling;plung;plrung;alat;alit;alut;alrut;alet;alot;alang;avidhiling;aashirling;alung;alrung"
+    
+
+    @classmethod
+    def _get_lakaar_name(cls, lakaar_code):
+        lakaar_dict = {
+            "plat": "लट्लकारः (परस्मैपदम्)",
+            "plit": "लिट्लकारः (परस्मैपदम्)",
+            "plut": "लुट्लकारः (परस्मैपदम्)",
+            "plrut": "लृट्लकारः (परस्मैपदम्)",
+            "plet": "लेट्लकारः (परस्मैपदम्)",
+            "plot": "लोट्लकारः (परस्मैपदम्)",
+            "plang": "लङ्लकारः (परस्मैपदम्)",
+            "pvidhiling": "विधिलिङ्लकारः (परस्मैपदम्)",
+            "pashirling": "आशीर्लिङ्लकारः (परस्मैपदम्)",
+            "plung": "लुङ्लकारः (परस्मैपदम्)",
+            "plrung": "लृङ्लकारः (परस्मैपदम्)",
+            "alat": "लट्लकारः (आत्मनेपदम्)",
+            "alit": "लिट्लकारः (आत्मनेपदम्)",
+            "alut": "लुट्लकारः (आत्मनेपदम्)",
+            "alrut": "लृट्लकारः (आत्मनेपदम्)",
+            "alet": "लेट्लकारः (आत्मनेपदम्)",
+            "alot": "लोट्लकारः (आत्मनेपदम्)",
+            "alang": "लङ्लकारः (आत्मनेपदम्)",
+            "avidhiling": "विधिलिङ्लकारः (आत्मनेपदम्)",
+            "aashirling": "आशीर्लिङ्लकारः (आत्मनेपदम्)",
+            "alung": "लुङ्लकारः (आत्मनेपदम्)",
+            "alrung": "लृङ्लकारः (आत्मनेपदम्)"
+        }
+        return lakaar_dict.get(lakaar_code, "")
